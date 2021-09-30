14d441a4fc9e014d224162bc2e20e3d0b3645095
Kamalakar Borlakunta <kamalakar07@gmail.com>
2021-09-30 17:07:13 -0500

    cleaned up javadoc

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -5,19 +5,6 @@ import org.springframework.stereotype.Service;
 
 /**
 * Some javadoc. // OK
-*
-* @author Some javadoc. // OK
-* @version Some javadoc. // OK
-* @param Some javadoc. // OK
-* @return Some javadoc. // OK
-* @throws Some javadoc. // OK
-* @exception Some javadoc. // OK
-* @see Some javadoc. // OK
-* @since Some javadoc. // OK
-* @serial Some javadoc. // OK
-* @serialField // OK
-* @serialData // OK
-* @deprecated Some javadoc. // OK
 */
 @Service
 public class Calculator {
