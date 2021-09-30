c3870889bca436ccbf798b6528942fa1062728b6
Kamalakar Borlakunta <kamalakar07@gmail.com>
2021-09-30 16:20:26 -0500

    added some java doc comment

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -1,3 +1,20 @@
+/**
+* Some javadoc. // OK
+*
+* @author Some javadoc. // OK
+* @version Some javadoc. // OK
+* @param Some javadoc. // OK
+* @return Some javadoc. // OK
+* @throws Some javadoc. // OK
+* @exception Some javadoc. // OK
+* @see Some javadoc. // OK
+* @since Some javadoc. // OK
+* @serial Some javadoc. // OK
+* @serialField // OK
+* @serialData // OK
+* @deprecated Some javadoc. // OK
+*/
+
 package com.leszko.calculator;
 
 import org.springframework.cache.annotation.Cacheable;
