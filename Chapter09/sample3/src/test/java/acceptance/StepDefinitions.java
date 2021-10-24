5dd4714714ddb656db0da53e97aa55332d823f4d
Kamalakar Borlakunta <kamalakar07@gmail.com>
2021-10-24 12:33:58 -0500

    1233

--- Chapter09/sample3/src/test/java/acceptance/StepDefinitions.java
+++ Chapter09/sample3/src/test/java/acceptance/StepDefinitions.java
@@ -16,9 +16,7 @@ public class StepDefinitions {
     private String a;
     private String b;
     private String result;
-    private String c;
-    private String d;
-    private String quotient;
+
 
     @Given("^I have two numbers: (.*) and (.*)$")
     public void i_have_two_numbers(String a, String b) throws Throwable {
@@ -37,20 +35,14 @@ public class StepDefinitions {
         assertEquals(expectedResult, result);
     }
 
-    @Given("^I have two numbers for division: (.*) and (.*)$")
-    public void i_have_two_numbers_for_division(String c, String d) throws Throwable {
-        this.c = c;
-        this.d = d;
-    }
-
     @When("^the calculator divides them$")
     public void the_calculator_divides_them() throws Throwable {
-        String url = String.format("%s/div?a=%s&b=%s", server, c, d);
-        quotient = restTemplate.getForObject(url, String.class);
+        String url = String.format("%s/div?a=%s&b=%s", server, a, b);
+        result = restTemplate.getForObject(url, String.class);
     }
 
     @Then("^I receive (.*) as the quotient$")
     public void i_receive_as_a_quotient(String expectedResult) throws Throwable {
-        assertEquals(expectedResult, quotient);
+        assertEquals(expectedResult, result);
     }
 }
